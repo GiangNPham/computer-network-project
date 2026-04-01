740f069d3792a607056eda3a1fd16ed4fe1a6d43
Giang Pham <giangpn25@gmail.com>
2026-04-01 15:24:34 -0500

    add comment + keep track of time

--- client.cpp
+++ client.cpp
@@ -1,103 +1,105 @@
-#include <iostream>
-#include <string>
 #include <algorithm>
-#include <regex>
-#include <unistd.h>
-#include <string.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <iostream>
 #include <netinet/in.h>
+#include <regex>
+#include <string.h>
+#include <string>
+#include <sys/socket.h>
+#include <unistd.h>
 
 using namespace std;
 
 #define MAXBUF 1024
 #define PORT 6767
 
-int main(int argc, char* argv[]) {
-    int sockfd;
-    char buffer[MAXBUF];
-    string openn = "Open ";
-    
-    openn += argv[1];
-    
-    const char* openMsg = openn.c_str();
-    struct sockaddr_in servaddr;
+int main(int argc, char *argv[]) {
+  int sockfd;
+  char buffer[MAXBUF];
+  string openn = "Open ";
 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
+  openn += argv[1];
+
+  const char *openMsg = openn.c_str();
+  // create a socket to communcate with the server
+  struct sockaddr_in servaddr;
+
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0) {
+    perror("socket creation failed");
+    exit(EXIT_FAILURE);
+  }
+
+  memset(&servaddr, 0, sizeof(servaddr));
+
+  // Fill server address info
+  servaddr.sin_family = AF_INET;                     // IPv4
+  servaddr.sin_port = htons(PORT);                   // Server port
+  servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server IP
+
+  socklen_t len = sizeof(servaddr);
+
+  // Send message to server
+  sendto(sockfd, openMsg, strlen(openMsg), MSG_CONFIRM,
+         (const struct sockaddr *)&servaddr, sizeof(servaddr));
+
+  // Receive reply from server
+  int n = recvfrom(sockfd, buffer, MAXBUF, MSG_WAITALL,
+                   (struct sockaddr *)&servaddr, &len);
+
+  buffer[n] = '\0'; // Null terminate received data
+  printf("%s\n", buffer);
+
+  regex expr_regex("^-?[0-9]+([.][0-9]+)?[-+*/]-?[0-9]+([.][0-9]+)?$");
+  string input;
+
+  while (true) {
+    cout << "Enter math expression (or 'quit' to exit): ";
+    if (!getline(cin, input))
+      break;
+
+    // Clean input: remove spaces
+    input.erase(remove(input.begin(), input.end(), ' '), input.end());
+
+    if (input == "quit") {
+      break;
     }
 
-    memset(&servaddr, 0, sizeof(servaddr));
-
-    // Fill server address info
-    servaddr.sin_family = AF_INET;              // IPv4
-    servaddr.sin_port   = htons(PORT);          // Server port
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server IP
-
-    socklen_t len = sizeof(servaddr);
-
-    // Send message to server
-    sendto(sockfd, openMsg, strlen(openMsg), MSG_CONFIRM,
-           (const struct sockaddr *)&servaddr, sizeof(servaddr));
-
-    // Receive reply from server
-    int n = recvfrom(sockfd, buffer, MAXBUF, MSG_WAITALL,
-                     (struct sockaddr *)&servaddr, &len);
-
-    buffer[n] = '\0';   // Null terminate received data
-    printf("%s\n", buffer);
-        
-    regex expr_regex("^-?[0-9]+([.][0-9]+)?[-+*/]-?[0-9]+([.][0-9]+)?$");
-    string input;
-    
-    while (true) {
-        cout << "Enter math expression (or 'quit' to exit): ";
-        if (!getline(cin, input)) break;
-
-        // Clean input: remove spaces
-        input.erase(remove(input.begin(), input.end(), ' '), input.end());
-
-        if (input == "quit") {
-            break;
-        }
-
-        // Verify format
-        if (!regex_match(input, expr_regex)) {
-            cout << "Invalid format. Please use a+b, a-b, a*b, or a/b (e.g., 2+3)." << endl;
-            continue;
-        }
-
-        // Send message to server
-        sendto(sockfd, input.c_str(), input.length(), MSG_CONFIRM,
-            (const struct sockaddr *)&servaddr, sizeof(servaddr));
-
-        // Receive reply from server
-        memset(buffer, 0, MAXBUF);
-        n = recvfrom(sockfd, buffer, MAXBUF, MSG_WAITALL,
-                        (struct sockaddr *)&servaddr, &len);
-
-        buffer[n] = '\0';   // Null terminate received data
-        printf("Result: %s\n\n", buffer);
+    // Verify format
+    if (!regex_match(input, expr_regex)) {
+      cout << "Invalid format. Please use a+b, a-b, a*b, or a/b (e.g., 2+3)."
+           << endl;
+      continue;
     }
 
-    const char* cl = "Close";
     // Send message to server
-    sendto(sockfd, cl, strlen(cl), MSG_CONFIRM,
+    sendto(sockfd, input.c_str(), input.length(), MSG_CONFIRM,
            (const struct sockaddr *)&servaddr, sizeof(servaddr));
 
     // Receive reply from server
     memset(buffer, 0, MAXBUF);
     n = recvfrom(sockfd, buffer, MAXBUF, MSG_WAITALL,
-                     (struct sockaddr *)&servaddr, &len);
+                 (struct sockaddr *)&servaddr, &len);
 
-    buffer[n] = '\0';   // Null terminate received data
-    printf("%s\n", buffer);
-    
+    buffer[n] = '\0'; // Null terminate received data
+    cout << buffer;
+  }
 
-    // Close socket
-    close(sockfd);
+  const char *cl = "Close";
+  // Send message to server
+  sendto(sockfd, cl, strlen(cl), MSG_CONFIRM,
+         (const struct sockaddr *)&servaddr, sizeof(servaddr));
 
-    return 0;
+  // Receive reply from server
+  memset(buffer, 0, MAXBUF);
+  n = recvfrom(sockfd, buffer, MAXBUF, MSG_WAITALL,
+               (struct sockaddr *)&servaddr, &len);
+
+  buffer[n] = '\0'; // Null terminate received data
+  printf("%s\n", buffer);
+
+  // Close socket
+  close(sockfd);
+
+  return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
-#include <sys/socket.h> 
-#include <sys/types.h>
 #include <arpa/inet.h>
-#include <stdlib.h>
-#include <cstring>
-#include <vector>
-#include <sstream>
 #include <chrono>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <vector>
 
 using namespace std;
 
@@ -14,157 +14,177 @@ using namespace std;
 #define PORT 6767
 
 struct client {
-    string name;
-    time_t connectedTime;
-    sockaddr_in addr;
-    bool valid;
-    // how long the user is attached to the server???
-    
-    client(string n, time_t t, sockaddr_in a) : name(n), connectedTime(t), addr(a), valid(true) {}
+  string name;
+  time_t connectedTime;
+  sockaddr_in addr;
+  bool valid;
+  double duration;
+
+  client(string n, time_t t, sockaddr_in a)
+      : name(n), connectedTime(t), addr(a), valid(true), duration(0) {}
 };
 
 // assume that the math operation is valid, and the end result can be large
-double calculateMath(char* buffer){ // input string here must be null-terminated, else need to have another input as the string length
-    double num1 = 0, num2 = 0;
-    char op = 0;
-    int i = 0;
-    if (buffer[i] == '-') i++;
-    while (buffer[i] && !(buffer[i] == '+' || buffer[i] == '-' || buffer[i] == '*' || buffer[i] == '/')) {
-        i++;
-    }
+double
+calculateMath(char *buffer) { // input string here must be null-terminated, else
+                              // need to have another input as the string length
+  double num1 = 0, num2 = 0;
+  char op = 0;
+  int i = 0;
+  if (buffer[i] == '-')
+    i++;
+  while (buffer[i] && !(buffer[i] == '+' || buffer[i] == '-' ||
+                        buffer[i] == '*' || buffer[i] == '/')) {
+    i++;
+  }
 
-    if (buffer[i]) {
-        op = buffer[i];
-        buffer[i] = '\0'; 
-        num1 = atof(buffer);
-        num2 = atof(buffer + i + 1);
-        buffer[i] = op; 
-    } else {
-        // Invalid format
-        return 0;
-    }
-    switch (op) {
-        case '+': return num1 + num2;
-        case '-': return num1 - num2;
-        case '*': return num1 * num2;
-        case '/': return num2 != 0 ? num1 / num2 : 0;
-        default: return 0;
-    }
+  if (buffer[i]) {
+    op = buffer[i];
+    buffer[i] = '\0';
+    num1 = atof(buffer);
+    num2 = atof(buffer + i + 1);
+    buffer[i] = op;
+  } else {
+    // Invalid format
+    return 0;
+  }
+  switch (op) {
+  case '+':
+    return num1 + num2;
+  case '-':
+    return num1 - num2;
+  case '*':
+    return num1 * num2;
+  case '/':
+    return num2 != 0 ? num1 / num2 : 0;
+  default:
+    return 0;
+  }
 }
 
-// the bind function binds a file descriptor to the SERVER address 
-// when the packet is received, the system can get the server address 
-// then see which file descriptor that it corresponds to 
+// the bind function binds a file descriptor to the SERVER address
+// when the packet is received, the system can get the server address
+// then see which file descriptor that it corresponds to
 // the server will read normally from a file descriptor
-int main(){
-    int socketfd;
-    char buffer[MAXBUF];
+int main() {
+  int socketfd;
+  char buffer[MAXBUF];
 
-    sockaddr_in serveraddr;
+  sockaddr_in serveraddr;
 
-    // create the socket fd
-    if ((socketfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
-        perror("creating socket failed\n");
-        exit(1);
+  // create the socket fd to listen to incoming requests
+  if ((socketfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    perror("creating socket failed\n");
+    exit(1);
+  }
+
+  memset(&serveraddr, 0, sizeof(serveraddr));
+
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_addr.s_addr = INADDR_ANY;
+  serveraddr.sin_port = htons(PORT);
+
+  if (bind(socketfd, (const sockaddr *)&serveraddr, sizeof(serveraddr)) < 0) {
+    perror("bind failed\n");
+    exit(1);
+  }
+
+  sockaddr_in clientaddr;
+  socklen_t len = sizeof(clientaddr);
+  vector<client> clients;
+
+  while (1) {
+    memset(&clientaddr, 0, sizeof(clientaddr));
+
+    // blocking operation: read the request
+    ssize_t bytesRead = recvfrom(socketfd, (char *)buffer, MAXBUF, MSG_WAITALL,
+                                 (sockaddr *)&clientaddr, &len);
+    buffer[bytesRead] = '\0';
+
+    int id = -1;
+
+    for (int i = 0; i < (int)clients.size(); i++) {
+      // if the address is in the vector and is valid (has not been closed)
+      client c = clients[i];
+      if (c.addr.sin_family == clientaddr.sin_family &&
+          c.addr.sin_port == clientaddr.sin_port &&
+          c.addr.sin_addr.s_addr == clientaddr.sin_addr.s_addr && c.valid) {
+        id = i;
+        break;
+      }
     }
 
+    if (id == -1) {
+      if (strncmp(buffer, "Open", 4) == 0) {
+        // create new entry for a new client
+        chrono::system_clock::time_point now = chrono::system_clock::now();
+        time_t now_c = chrono::system_clock::to_time_t(now);
 
-    memset(&serveraddr, 0, sizeof(serveraddr)); 
-      
-    serveraddr.sin_family    = AF_INET;
-    serveraddr.sin_addr.s_addr = INADDR_ANY; 
-    serveraddr.sin_port = htons(PORT);
+        clients.emplace_back(buffer + 5, now_c, clientaddr);
 
-    if (bind(socketfd, (const sockaddr*) &serveraddr, sizeof(serveraddr)) < 0){
-        perror("bind failed\n");
-        exit(1);
+        // send acknowledgement and write log
+
+        const char *successMessage = "Server has set up a connection!\n";
+        sendto(socketfd, successMessage, strlen(successMessage), MSG_CONFIRM,
+               (sockaddr *)&clientaddr, len);
+
+        char *timeString = std::ctime(&now_c);
+
+        cout << "Client " << clients.back().name << " opened a connection at "
+             << timeString;
+      } else {
+        // if client has not connected, send back an Error message
+        const char *invalidMessage =
+            "Client has not set up connection with server!\n";
+        sendto(socketfd, invalidMessage, strlen(invalidMessage), MSG_CONFIRM,
+               (sockaddr *)&clientaddr, len);
+      }
+      continue;
     }
 
-    // use a queue to answer the request in FIFO order
-    // 3 types of request: open connection, math operation, close connection
+    // log out detail of the request
+    cout << "Client " << clients[id].name << " sent a request: " << buffer
+         << "\n";
 
-    /*
-    Request format:
-    1. Open [name]
-    2. Math operation
-    3. Close
-    */
+    if (strncmp(buffer, "Open", 4) == 0) {
+      // if the client has already opened a connection then send an error
+      // message
+      const char *invalidMessage = "Already opened a connection!\n";
+      sendto(socketfd, invalidMessage, strlen(invalidMessage), MSG_CONFIRM,
+             (sockaddr *)&clientaddr, len);
 
-    // use a vector to keep track of all the users
-    // create a struct
+    } else if (strcmp(buffer, "Close") == 0) {
+      // clear the entry for the closing client
+      clients[id].valid = false;
+      time_t now_c =
+          chrono::system_clock::to_time_t(chrono::system_clock::now());
+      clients[id].duration = difftime(now_c, clients[id].connectedTime);
 
-    sockaddr_in clientaddr;
-    socklen_t len = sizeof(clientaddr);
-    vector<client> clients;
+      const char *closeMessage = "Server has closed the connection!\n";
+      sendto(socketfd, closeMessage, strlen(closeMessage), MSG_CONFIRM,
+             (sockaddr *)&clientaddr, len);
 
-    while (1){
-        memset(&clientaddr, 0, sizeof(clientaddr)); 
+      if (clients[id].duration > 60) {
+        cout << "Client " << clients[id].name << " was connected for "
+             << clients[id].duration / 60.0 << " minutes.\n";
+      } else {
+        cout << "Client " << clients[id].name << " was connected for "
+             << clients[id].duration << " seconds.\n";
+      }
+    } else if (isdigit(buffer[0]) || buffer[0] == '-') {
+      // calculate the result and send back to the requesting client
+      double result = calculateMath(buffer);
+      stringstream ss;
+      ss << "Result is " << result << "\n";
+      string message = ss.str();
 
-        // blocking operation
-        ssize_t bytesRead = recvfrom(socketfd, (char *) buffer, MAXBUF, MSG_WAITALL, (sockaddr*) &clientaddr, &len);
-        buffer[bytesRead] = '\0';
-
-        int id = -1;
-
-        for (int i = 0; i < (int)clients.size(); i++){
-            // if the address is in the vector and is valid (has not been closed)
-            client c = clients[i];
-            if (c.addr.sin_family == clientaddr.sin_family && c.addr.sin_port == clientaddr.sin_port && c.addr.sin_addr.s_addr == clientaddr.sin_addr.s_addr && c.valid){
-                id = i;
-                break;
-            }
-        }
-
-
-        if (id == -1){
-            if (strncmp(buffer, "Open", 4) == 0) {
-                // create new entry for a new client
-                chrono::system_clock::time_point now = chrono::system_clock::now();
-                time_t now_c = chrono::system_clock::to_time_t(now);
-
-                clients.emplace_back(buffer + 5, now_c, clientaddr);
-
-                // send acknowledgement and log out
-
-                const char*successMessage = "Server has set up a connection!\n";
-                sendto(socketfd, successMessage, strlen(successMessage), MSG_CONFIRM, (sockaddr *) &clientaddr, len);
-
-                char* timeString = std::ctime(&now_c); 
-
-                cout << "Client " << clients.back().name << " opened a connection at " << timeString;
-            } else {
-                const char*invalidMessage = "Client has not set up connection with server!\n";
-                sendto(socketfd, invalidMessage, strlen(invalidMessage), MSG_CONFIRM, (sockaddr *) &clientaddr, len);
-            }
-            continue;
-        }
-
-        // log out detail of the request
-        cout << "Client " << clients[id].name << " sent a request: " << buffer << "\n";
-
-        if (strncmp(buffer, "Open", 4) == 0){
-            // if the client has already opened a connection then send an error message
-            const char*invalidMessage = "Already opened a connection!\n";
-            sendto(socketfd, invalidMessage, strlen(invalidMessage), MSG_CONFIRM, (sockaddr *) &clientaddr, len);
-
-        } else if (strcmp(buffer, "Close") == 0) {
-            clients[id].valid = false;
-            const char*closeMessage = "Server has closed the connection!\n";
-            sendto(socketfd, closeMessage, strlen(closeMessage), MSG_CONFIRM, (sockaddr *) &clientaddr, len);
-
-        } else if (isdigit(buffer[0]) || buffer[0] == '-'){
-            double result = calculateMath(buffer);
-            stringstream ss;
-            ss << "Result is " << result << "\n";
-            string message = ss.str();
-
-            sendto(socketfd, (char *)message.c_str(), message.length(), MSG_CONFIRM, (sockaddr *) &clientaddr, len);
-        } else {
-            const char*invalidMessage = "Request is invalid, try again!\n";
-            sendto(socketfd, invalidMessage, strlen(invalidMessage), MSG_CONFIRM, (sockaddr *) &clientaddr, len);
-        }
-        
+      sendto(socketfd, (char *)message.c_str(), message.length(), MSG_CONFIRM,
+             (sockaddr *)&clientaddr, len);
+    } else {
+      // send back an Error message for invalid requests
+      const char *invalidMessage = "Request is invalid, try again!\n";
+      sendto(socketfd, invalidMessage, strlen(invalidMessage), MSG_CONFIRM,
+             (sockaddr *)&clientaddr, len);
     }
-
-
+  }
 }
